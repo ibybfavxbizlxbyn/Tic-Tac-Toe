80a4ad59cd32fdf1df1d4bc2ccb87c2f4051e18a
IbybfavxbiZlxbyn <volosnikovkolia@gmail.com>
2017-01-06 18:53:38 +0200

    Low level AI add.

--- main.cpp
+++ main.cpp
@@ -10,22 +10,30 @@ char pole[3][3];
 char name1[10];
 char name2[10];
 
-void start();
-bool input();
+int start();
+bool input(int AI);
 bool win();
 void print(bool header);
 
 int main()
 {
     srand(time(NULL));
-    start();
+    const unsigned short mode = start();
     system("cls");
     print(true);
 
+    cin.get();
     cout << "Enter name of first player: ";
-    cin.getline(name1, 30);
-    cout << "Enter name of second player: ";
-    cin.getline(name2, 30);
+    cin.getline(name1, 10);
+    if (mode == 0)
+    {
+        cout << "Enter name of second player: ";
+        cin.getline(name2, 30);
+    } else
+    {
+        name2 = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r'};
+    }
+
     system("cls");
     print(true);
     if (rand() % 2 == 0)
@@ -39,13 +47,41 @@ int main()
     }
     _getch();
 
+    unsigned short step_num = 0;
     while (!win())
     {
+        if (step_num == 10)
+            break;
         system("cls");
-        if (!input())
+        if (mode == 0)
         {
-            cout << "Error: wrong input of data;" << endl;
-            _getch();
+            if (!input(0))
+            {
+                cout << "Error: wrong input of data;" << endl;
+                _getch();
+            }
+        } else
+        {
+            if (step)
+            {
+                int k;
+                while (!input(k))
+                {
+                    if (mode == 1)
+                    {
+                        k = rand()%10;
+                    } else
+                    {
+                        cout << "Error: 404" << endl;
+                    }
+                }
+            } else
+            {
+                if (!input(0))
+                {
+                    cout << "Error: wrong input of data;" << endl;
+                }
+            }
         }
     }
 
@@ -86,9 +122,9 @@ void print(bool header)
     }
 }
 
-void start()
+int start()
 {
-    int l = 0;
+        int l = 0;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++, l++)
@@ -96,21 +132,81 @@ void start()
             pole[i][j] = 49+l;
         }
     }
+    while (true)
+    {
+        cout << "\t\t*** Tic Tac Toe ***\n\n";
+        cout << "1. Player vs AI;" << endl;
+        cout << "2. Players vs player" << endl;
+        cout << "Chose game mode: ";
+        unsigned short choise;
+        cin >> choise;
+        if (choise == 1)
+        {
+            choise = 0;
+            while (true)
+            {
+                system("cls");
+                cout << "\t\t*** Tic Tac Toe ***\n\n";
+                cout << "1. Player vs AI;" << endl;
+                cout << "   1)Low level AI;" << endl;
+                cout << "   2)High level AI;" << endl;
+                cout << "2. Players vs player" << endl;
+                cout << "Chose AI level: ";
+                cin >> choise;
+                if (choise != 1 && choise != 2)
+                {
+                    cout << "Error: wromg input of data;" << endl;
+                    _getch();
+                    system("cls");
+                } else
+                    break;
+            }
 
-    cout << "\t\t*** Tic Tac Toe ***\n\n";
-    cout << "   Rules: " << endl;
-    cout << "You play with computer on game pole 3x3." << endl;
-    cout << "Wins the one who make a winning combination." << endl;
-    cout << "Game pole: " << endl << endl;
-    print(false);
-    cout << endl;
-    cout << "To make a step press button with a number of a pole." << endl;
-    cout << "Press any key to start" << endl;
-    _getch();
-    system("cls");
+            system("cls");
+            cout << "\t\t*** Tic Tac Toe ***\n\n";
+            cout << "   Rules: " << endl;
+            cout << "You play with computer on game pole 3x3." << endl;
+            cout << "Wins the one who make a winning combination." << endl;
+            cout << "Game pole: " << endl << endl;
+            print(false);
+            cout << endl;
+            cout << "To make a step press button with a number of a pole." << endl;
+            cout << "Press any key to start" << endl;
+            _getch();
+            system("cls");
+            if (choise == 1)
+            {
+                return  1;
+            } else
+            {
+                return 2;
+            }
+        } else
+        if (choise == 2)
+        {
+            system("cls");
+            cout << "\t\t*** Tic Tac Toe ***\n\n";
+            cout << "   Rules: " << endl;
+            cout << "2 users play on game pole 3x3." << endl;
+            cout << "Wins the one who make a winning combination." << endl;
+            cout << "Game pole: " << endl << endl;
+            print(false);
+            cout << endl;
+            cout << "To make a step press button with a number of a pole." << endl;
+            cout << "Press any key to start" << endl;
+            _getch();
+            system("cls");
+            return 0;
+        } else
+        {
+            cout << "Error: wrong input of data;" << endl;
+            _getch();
+            system("cls");
+        }
+    }
 }
 
-bool input ()
+bool input (int AI)
 {
     system("cls");
     print(true);
@@ -119,12 +215,25 @@ bool input ()
     {
         cout << name1 << " go: ";
     } else
+    if (!AI)
     {
         cout << name2 << " go: ";
+    } else
+    {
+        cout << "Computer go." << endl;
+        _getch();
     }
+
     int i, j;
     int k;
-    cin >> k;
+    if (!AI)
+    {
+        cin >> k;
+    } else
+    {
+        k = AI;
+    }
+
     if (k < 1 || k > 9)
         return false;
 
@@ -151,6 +260,7 @@ bool input ()
         step = false;
     }
 
+    cin.get();
     return true;
 }
 
